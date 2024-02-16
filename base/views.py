7229b023d76407e25222927d97971ac5e8c0a1bc
kibeert <ckibertkoech@gmail.com>
2024-02-16 11:43:07 +0300

    Add customuser form to register view. (generated by blackbox.ai )

--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import customuser
 
 # Create your views here.
+def register(request, *args, **kwargs):
+    form = customuser
+    context = {
+        'form':form
+    }
+    return render(request, "register.html", context)
